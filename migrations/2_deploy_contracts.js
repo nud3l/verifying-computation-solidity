c489fd91fcbee50d2b5a91f6fc80164c1a495975
nud3l <dominik.harz@gmail.com>
2017-03-29 13:43:39 +0200

    Include migrations contract and updated migration scripts

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,8 @@
+// var usingOraclize = artifacts.require("./usingOraclize.sol");
+var ComputationService = artifacts.require("./ComputationService.sol");
+
 module.exports = function(deployer) {
-  deployer.deploy(usingOraclize);
-  deployer.autolink();
+  //deployer.deploy(usingOraclize);
+  //deployer.link(ConvertLib, MetaCoin);
   deployer.deploy(ComputationService);
 };
---
+++ test/computationservice.js
@@ -0,0 +1,7 @@
+// Specifically request an abstraction for MetaCoin
+var ComputationService = artifacts.require("ComputationService");
+
+contract('ComputationService', function(accounts) {
+
+
+});
