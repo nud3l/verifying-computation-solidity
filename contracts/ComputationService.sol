163831cb48a8d9df538d4133212f7b786e822210
nud3l <dominik.harz@gmail.com>
2017-03-30 11:56:03 +0200

    Include helpers, simple test and update contract

--- contracts/ComputationService.sol
+++ contracts/ComputationService.sol
@@ -2,19 +2,31 @@ pragma solidity ^0.4.8;
 import "./usingOraclize.sol";
 
 contract ComputationService is usingOraclize {
-  bytes32 result;
+  uint public result;
+  bytes32 public oraclizeID;
+
+  event newOraclizeQuery(string description);
+  event newResult(string comp_result);
+
   string URL = "https://r98ro6hfj5.execute-api.eu-west-1.amazonaws.com/test/int";
-  string JSON = '{"val1": 4, "val2": 7}';
+  string JSON = '{"val1": 4, "val2": 9}';
   // constructor
   function ComputationService() {
     OAR = OraclizeAddrResolverI(0x6f485C8BF6fc43eA212E93BBF8ce046C7f1cb475);
   }
 
-  function IntMultiplication() {
-    result = oraclize_query("URL", URL, JSON);
+  function __callback(bytes32 _oraclizeID, string _result) {
+    if (msg.sender != oraclize_cbAddress()) throw;
+    newResult(_result);
+    result = parseInt(_result);
   }
 
-  function getResult() constant returns (bytes32) {
+  function multiply() payable {
+    newOraclizeQuery("Oraclize query was sent, standing by for the answer.");
+    oraclize_query(60, "URL", URL, JSON);
+  }
+
+  function getResult() constant returns (uint) {
     return result;
   }
 }
--- test/computationservice.js
+++ test/computationservice.js
@@ -2,6 +2,18 @@
 var ComputationService = artifacts.require("ComputationService");
 
 contract('ComputationService', function(accounts) {
+  it("should store 28 as result", function() {
+    var meta;
+    var computation;
 
-
+    return ComputationService.deployed().then(function(instance) {
+      meta = instance;
+      return meta.IntMultiplication.call(accounts[0]);
+    }).then(function() {
+      return meta.getResult.call(accounts[0]);
+    }).then(function(result) {
+      computation = result;
+      assert.equal(computation, 28, "The result wasn't 28");
+    });
+  });
 });
