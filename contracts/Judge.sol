1c38d1efbaff7a23988045e7b2ffd5af8c738d80
nud3l <dominik.harz@gmail.com>
2017-04-20 11:50:24 +0200

    Rename variables

--- contracts/Judge.sol
+++ contracts/Judge.sol
@@ -4,15 +4,15 @@ contract Judge {
   function resolveDispute(uint _input1, uint _input2, uint _result, uint8 _operation) returns (bool check) {
     uint check_result;
 
-    if (operation == 0) {
+    if (_operation == 0) {
       check_result = _input1 + _input2;
       if (check_result == _result) check = true;
     }
-    else if (operation == 1) {
+    else if (_operation == 1) {
       check_result = _input1 - _input2;
       if (check_result == _result) check = true;
     }
-    else if (operation== 2) {
+    else if (_operation== 2) {
       check_result = _input1 * _input2;
       if (check_result == _result) check = true;
     }
