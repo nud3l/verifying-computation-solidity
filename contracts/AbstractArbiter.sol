346dccbd79cfab9cb471f0964f2589c1bc82bfa1
nud3l <dominik.harz@gmail.com>
2017-04-22 13:58:34 +0200

    Update functions

--- contracts/AbstractArbiter.sol
+++ contracts/AbstractArbiter.sol
@@ -1,15 +1,20 @@
 pragma solidity ^0.4.8;
 
-contract Arbiter {
+contract AbstractArbiter {
   function enableService();
 
   function disableService();
 
-  function requestComputation(string _input1, string _input2, uint _operation);
+  function requestComputation(string _input1, string _input2, uint _operation, uint _numVerifiers);
 
-  function executeComputation(uint _numVerifiers) payable;
+  function executeComputation() payable;
 
-  function rand(uint min, uint max) constant returns (uint256);
+  function receiveResults(string _result, address _origin);
 
-  function __callback();
+  function compareResults() returns (uint);
+
+  function requestIndex();
+
+  function receiveIndex(uint _index1, uint _index2, uint _operation, address _origin, bool _end);
+
 }
