040e15495dc5802754f44a26e86ec52f9c0ab1aa
nud3l <dominik.harz@gmail.com>
2017-04-25 14:08:55 +0200

    Update migration script

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,11 +1,21 @@
 // var usingOraclize = artifacts.require("./usingOraclize.sol");
+var Arbiter = artifacts.require("./Arbiter.sol");
+var Judge = artifacts.require("./Judge.sol");
 var ComputationService = artifacts.require("./ComputationService.sol");
 
 module.exports = function(deployer) {
   //deployer.deploy(usingOraclize);
   //deployer.link(ConvertLib, MetaCoin);
   var contracts = [];
-  for (i = 0; i < 2; i++) {
+
+  // deploy one Arbiter
+  contracts.push(Arbiter);
+
+  // deploy one Judge
+  contracts.push(Judge);
+
+  // deploy six computation services
+  for (i = 0; i < 6; i++) {
     contracts.push(ComputationService);
   }
 
