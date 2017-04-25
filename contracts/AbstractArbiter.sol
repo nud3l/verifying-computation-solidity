cbb7a5ffaf62404d548c70fabde17f3abd3cebc6
nud3l <dominik.harz@gmail.com>
2017-04-25 13:17:38 +0200

    Update abstract contract ABIs

--- contracts/AbstractArbiter.sol
+++ contracts/AbstractArbiter.sol
@@ -1,6 +1,7 @@
 pragma solidity ^0.4.8;
 
 contract AbstractArbiter {
+
   function enableService();
 
   function disableService();
@@ -9,12 +10,18 @@ contract AbstractArbiter {
 
   function executeComputation() payable;
 
-  function receiveResults(string _result, address _origin);
+  function receiveResults(string _result, uint256 _computationId);
 
   function compareResults() returns (uint);
 
   function requestIndex();
 
-  function receiveIndex(uint _index1, uint _index2, uint _operation, address _origin, bool _end);
+  function receiveIndex(uint _index1, uint _index2, uint _operation, uint256 _computationId, bool _end);
+
+  function stringToUint(string s) internal constant returns (uint result);
+
+  function stringsEqual(string _a, string _b) internal constant returns (bool);
+
+  function rand(uint min, uint max) internal constant returns (uint256);
 
 }
--- contracts/AbstractComputationService.sol
+++ contracts/AbstractComputationService.sol
@@ -3,7 +3,9 @@ pragma solidity ^0.4.8;
 contract AbstractComputationService {
   function __callback(bytes32 _oraclizeID, string _result);
 
-  function compute(string _val1, string _val2, uint _operation, address _origin) payable;
+  function compute(string _val1, string _val2, uint _operation, uint256 _computationId) payable;
+
+  function provideIndex(string _resultSolver, uint _computationId);
 
   function registerOperation(uint _operation) payable;
 
