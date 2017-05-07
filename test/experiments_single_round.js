fa07d6e29d8946b675fcf6537fae86d2553655bc
nud3l <dominik.harz@gmail.com>
2017-05-07 17:44:19 +0200

    Include experiment tests

---
+++ test/experiments_single_round.js
@@ -0,0 +1,64 @@
+var Arbiter = artifacts.require("Arbiter");
+var ComputationService = artifacts.require("ComputationService");
+
+contract('Experiments', function(accounts) {
+  it("Full process: 1 cheater, 2 honest computation services", function(done) {
+    // register computation[0] to correct and incorrect ones
+    var arbiter, status, result, solver;
+
+    Arbiter.deployed().then(function(instance) {
+      arbiter = instance;
+      return arbiter.requestComputation("43543", "423543543", 0, 2, {from:accounts[0], gas: 4710000});
+    }).then(function() {
+      return arbiter.getStatus(accounts[0]);
+    }).then(function(result) {
+      status = result;
+      assert.equal(status, 100, "Computation request creation failed");
+      return arbiter.executeComputation({from:accounts[0], gas: 4712388, value: web3.toWei(0.10, "ether")});
+    }).then(function() {
+      return arbiter.getStatus(accounts[0]);
+    }).then(function(result) {
+      status = result;
+      assert.equal(status, 200, "Computation execution failed");
+    }).then(function(){
+      return new Promise(resolve => setTimeout(resolve, 130000));
+    }).then(function(){
+      return arbiter.getStatus(accounts[0]);
+    }).then(function(received){
+      status = received;
+      assert.equal(status, 400, "Not all results are in.");
+    }).catch(function(e){
+      console.log(e);
+    }).then(function(){
+      return arbiter.compareResults(accounts[0], {from:accounts[0], gas: 200000});
+    }).then(function(){
+      return arbiter.getStatus(accounts[0]);
+    }).then(function(issue){
+      status = issue;
+      assert.isAtLeast(status, 700, "Should have identified a mismatch");
+      done();
+    });
+    // .then(function(){
+    //   arbiter.requestIndex({from:accounts[0], gas: 100000});
+    //   return arbiter.getStatus(accounts[0]);
+    // }).then(function(resolution){
+    //   status = resolution;
+    //   assert.equal(status, 800, "Dispute resolution not started.");
+    // }).then(function(){
+    //   return new Promise(resolve => setTimeout(resolve, 2000));
+    // }).then(function(){
+    //   return arbiter.getCurrentSolver(accounts[0]);
+    // }).then(function(thisSolver){
+    //   solver = thisSolver;
+    //   return arbiter.getStatus(accounts[0]);
+    // }).then(function(solution){
+    //   status = solution;
+    //   if (solver = address) {
+    //     assert.equal(status, 901, "Dispute not resolved or did not detect that solver was wrong");
+    //   } else {
+    //     assert.equal(status, 902, "Dispute not resolved or did not detect that verifier was wrong");
+    //   }
+    //   done();
+    // });
+  });
+});
