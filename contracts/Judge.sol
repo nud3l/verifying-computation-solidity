ce4d22ab0837a50b929334516f437f421bf6e7a9
nud3l <dominik.harz@gmail.com>
2017-05-08 12:21:35 +0200

    Change order of computations

--- contracts/Judge.sol
+++ contracts/Judge.sol
@@ -5,15 +5,15 @@ contract Judge {
     uint check_result;
 
     if (_operation == 0) {
-      check_result = _input1 + _input2;
+      check_result = _input1 * _input2;
       if (check_result == _result) check = true;
     }
     else if (_operation == 1) {
-      check_result = _input1 - _input2;
+      check_result = _input1 + _input2;
       if (check_result == _result) check = true;
     }
     else if (_operation== 2) {
-      check_result = _input1 * _input2;
+      check_result = _input1 - _input2;
       if (check_result == _result) check = true;
     }
     return check;
